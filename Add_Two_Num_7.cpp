6a4898955e9fd69d0764c861216e442dc9c53a7d
Hyebin Lee <75057786+splopsky@users.noreply.github.com>
2022-03-26 23:55:59 +0900

    Create Add_Two_Num_7.cpp

---
+++ Add_Two_Num_7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+  int T;
+  cin >> T;
+
+  int* add = new int[T*2];
+
+  for (int i = 0; i < T*2; i++){
+    cin >> add[i];
+  }
+  
+  for (int i = 0; i < T; i++){
+    int k = i * 2;
+    cout << "Case #" << i + 1 << ": " << add[k] + add[k+1] << endl;
+  }
+
+  delete[] add;
+
+  
+  return 0;
+  
+}
