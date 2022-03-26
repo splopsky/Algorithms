060d5366df178adf90fb9bbe2813b6317e0add2c
Hyebin Lee <75057786+splopsky@users.noreply.github.com>
2022-03-26 23:44:37 +0900

    Create Faster_A_Plus_B.cpp

---
+++ Faster_A_Plus_B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+  int T;
+  int a, b;
+  cin >> T;
+
+  cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+  for(int i = 0; i < T; i++){
+    cin >> a >> b;
+    cout << a + b << "\n";
+  }
+  
+  return 0;
+  
+}
