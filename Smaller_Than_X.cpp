3bf5064bb98f1c3206811de1743a0b4cc8ac3b59
Hyebin Lee <75057786+splopsky@users.noreply.github.com>
2022-03-28 00:11:55 +0900

    Create Smaller_Than_X.cpp

---
+++ Smaller_Than_X.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n;
+  int x;
+
+  cin >> n >> x;
+  
+  int* arr = new int[n];
+
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+
+   for (int i = 0; i < n; i++){
+     if (arr[i] < x) {
+       cout << arr[i] << " ";
+     }
+   }
+  
+  
+  return 0;
+
+}
